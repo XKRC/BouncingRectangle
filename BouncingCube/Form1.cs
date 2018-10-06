87e98246154f7da887c7bb59a567d6c057e26cfe
Xinke Cao <caox1@unlv.nevada.edu>
2018-10-05 17:03:11 -0700

    added threashold

---
+++ BouncingCube/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace BouncingCube
+{
+    public partial class Form1 : Form
+    {
+
+        int characterXLeft;
+        int characterXRight;
+        int characterYTop;
+        int characterYBottom;
+        int characterWidth;
+        int characterHeight;
+        int xLeftBound;
+        int xRightBound;
+        int yTopBound;
+        int yBottomBound;
+
+        int characterVx;
+        int characterVy;
+
+        int deltaX;
+        int deltaY;
+
+        int threashold; //the threashold will be one pixle 
+        Pen blackPen = new Pen(Color.FromArgb(255, 0, 0, 0),3);
+        Graphics character;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            threashold = 2;
+            //let the character start on the middle left
+            characterXLeft = 0 + threashold;
+            characterWidth = outputPic.ClientSize.Height / 20;
+            characterXRight = characterWidth + characterXLeft;
+            characterYTop = outputPic.ClientSize.Height / 2;
+            characterHeight = outputPic.ClientSize.Height / 20;
+            characterYBottom = characterHeight + characterYTop;
+            outputPic.Invalidate();
+            deltaX = 1;
+            deltaY = 1;
+
+            //double buffering technique
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer, true);
+            this.UpdateStyles();
+        }
+
+
+        void projectNextFrame()
+        {
+            //finds where the box will move next
+        }
+
+        private void outputPic_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawRectangle(blackPen, characterXLeft, characterYTop, characterXRight - characterXLeft, characterYBottom - characterYTop);
+        }
+
+        private void moveFrame()
+        {
+
+                characterXLeft += deltaX;
+                characterXRight = characterWidth + characterXLeft;
+                characterYTop += deltaY;
+                characterYBottom = characterHeight + characterYTop;
+                //character.FillRectangle(blackPen, new Rectangle(characterXLeft, characterYTop, characterXRight - characterXLeft, characterYBottom - characterYTop));
+      
+            if (characterXLeft < 0 + threashold)
+            {
+                deltaX = 1;
+                characterXLeft = threashold;
+                characterXRight = characterWidth + characterXLeft;
+                //Debug.WriteLine("Has hit the left");
+
+            }
+
+            if (characterXRight > outputPic.ClientSize.Width - threashold)
+            {
+                deltaX = -1;
+                characterXRight = outputPic.ClientSize.Width - threashold;
+                characterXLeft = characterXRight - characterWidth;
+                //Debug.WriteLine("Has hit the right");
+
+            }
+
+            if (characterYTop < 0 + threashold)
+            {
+                deltaY = 1;
+                characterYTop = threashold;
+                characterYBottom = characterHeight + characterYTop;
+                //Debug.WriteLine("Has hit the top");
+            }
+
+            if (characterYBottom > outputPic.ClientSize.Height - threashold)
+            {
+                deltaY = -1;
+                characterYBottom = outputPic.ClientSize.Height - threashold;
+                characterYTop = characterYBottom - characterHeight;
+                //Debug.WriteLine("Has hit the bottom");
+            }
+        }
+
+        private void outputPic_Click(object sender, EventArgs e)
+        {
+            moveFrame();
+        }
+
+
+        //1000 interval = 1000ms = 1s
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            moveFrame();
+            outputPic.Invalidate();  //calls the paint event
+        }
+    }
+}
---
+++ BouncingCube/Matrix3x3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Matrix3x3
+{
+    double a11, a12, a13, a21, a22, a23, a31, a32, a33;
+
+    public Matrix3x3()
+	{
+        a11 = 0;
+        a12 = 0;
+        a13 = 0;
+        a21 = 0;
+        a22 = 0;
+        a23 = 0;
+        a31 = 0;
+        a32 = 0;
+        a33 = 0;
+	}
+
+    public Matrix3x3(double a11, double a12, double a13, double a21, double a22, double a23, double a31, double a32, double a33)
+    {
+        this.a11 = a11;
+        this.a12 = a12;
+        this.a13 = a13;
+        this.a21 = a21;
+        this.a22 = a22;
+        this.a23 = a23;
+        this.a31 = a31;
+        this.a32 = a32;
+        this.a33 = a33;
+    }
+
+    Matrix3x3 times(Matrix3x3 m)
+    {
+        return new Matrix3x3(this.a11 * m.a11 + this.a12 * m.a21 + this.a13 * m.a31, this.a11 * m.a12 + this.a12 * m.a22 + this.a13 * m.a32, this.a11 * m.a13 + this.a12 * m.a23 + this.a13 * m.a33,
+                          this.a21 * m.a11 + this.a22 * m.a21 + this.a23 * m.a31, this.a21 * m.a12 + this.a22 * m.a22 + this.a23 * m.a32, this.a21 * m.a13 + this.a22 * m.a23 + this.a23 * m.a33,
+                          this.a31 * m.a11 + this.a32 * m.a21 + this.a33 * m.a31, this.a31 * m.a12 + this.a32 * m.a22 + this.a33 * m.a32, this.a31 * m.a13 + this.a32 * m.a23 + this.a33 * m.a33);
+    }
+}
+
